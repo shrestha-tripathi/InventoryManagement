9820be3b4c0b85cba73a8647113fc2769d36a95a
shrestha tripathi <shrestha.tripathi@gmail.com>
2023-12-13 02:56:30 +0530

    Completed Sales page - bill print pending

--- sales.Designer.cs
+++ sales.Designer.cs
@@ -29,15 +29,33 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            textBox1 = new TextBox();
-            comboBox1 = new ComboBox();
-            textBox2 = new TextBox();
             dateTimePicker1 = new DateTimePicker();
+            textBox2 = new TextBox();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            panel2 = new Panel();
+            button3 = new Button();
+            listBox1 = new ListBox();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            dataGridView1 = new DataGridView();
+            label9 = new Label();
+            label10 = new Label();
+            button1 = new Button();
+            button2 = new Button();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -55,37 +73,40 @@
             panel1.Size = new Size(1823, 164);
             panel1.TabIndex = 0;
             // 
-            // label1
+            // dateTimePicker1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(18, 49);
-            label1.Name = "label1";
-            label1.Size = new Size(144, 37);
-            label1.TabIndex = 0;
-            label1.Text = "Firstname";
+            dateTimePicker1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            dateTimePicker1.Location = new Point(1381, 41);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(340, 43);
+            dateTimePicker1.TabIndex = 7;
             // 
-            // label2
+            // textBox2
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(386, 46);
-            label2.Name = "label2";
-            label2.Size = new Size(140, 37);
-            label2.TabIndex = 1;
-            label2.Text = "Lastname";
-            label2.Click += label2_Click;
+            textBox2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(532, 43);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 43);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
-            // label3
+            // comboBox1
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(775, 43);
-            label3.Name = "label3";
-            label3.Size = new Size(127, 37);
-            label3.TabIndex = 2;
-            label3.Text = "Bill Type";
-            label3.Click += label3_Click;
+            comboBox1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "CASH", "DEBIT" });
+            comboBox1.Location = new Point(908, 43);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(242, 45);
+            comboBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(168, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 43);
+            textBox1.TabIndex = 4;
             // 
             // label4
             // 
@@ -98,52 +119,229 @@
             label4.Text = "Bill Date";
             label4.Click += label4_Click;
             // 
-            // textBox1
+            // label3
             // 
-            textBox1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            textBox1.Location = new Point(168, 46);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(200, 43);
-            textBox1.TabIndex = 4;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(775, 43);
+            label3.Name = "label3";
+            label3.Size = new Size(127, 37);
+            label3.TabIndex = 2;
+            label3.Text = "Bill Type";
+            label3.Click += label3_Click;
             // 
-            // comboBox1
+            // label2
             // 
-            comboBox1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "CASH", "DEBIT" });
-            comboBox1.Location = new Point(908, 43);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(242, 45);
-            comboBox1.TabIndex = 5;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(386, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(140, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Lastname";
+            label2.Click += label2_Click;
             // 
-            // textBox2
+            // label1
             // 
-            textBox2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            textBox2.Location = new Point(532, 43);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(200, 43);
-            textBox2.TabIndex = 6;
-            textBox2.TextChanged += textBox2_TextChanged;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(18, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Firstname";
             // 
-            // dateTimePicker1
+            // panel2
             // 
-            dateTimePicker1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            dateTimePicker1.Location = new Point(1381, 41);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(340, 43);
-            dateTimePicker1.TabIndex = 7;
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(listBox1);
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(label5);
+            panel2.Location = new Point(25, 200);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1823, 300);
+            panel2.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(1565, 148);
+            button3.Name = "button3";
+            button3.Size = new Size(236, 46);
+            button3.TabIndex = 13;
+            button3.Text = "Add";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 32;
+            listBox1.Location = new Point(168, 106);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(240, 164);
+            listBox1.TabIndex = 12;
+            listBox1.Visible = false;
+            listBox1.KeyDown += listBox1_KeyDown;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox6.Location = new Point(1390, 38);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(200, 43);
+            textBox6.TabIndex = 11;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(1302, 41);
+            label8.Name = "label8";
+            label8.Size = new Size(82, 37);
+            label8.TabIndex = 10;
+            label8.Text = "Total";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox5.Location = new Point(925, 41);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(200, 43);
+            textBox5.TabIndex = 9;
+            textBox5.Leave += textBox5_Leave;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(790, 44);
+            label7.Name = "label7";
+            label7.Size = new Size(129, 37);
+            label7.TabIndex = 8;
+            label7.Text = "Quantity";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox4.Location = new Point(532, 44);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(200, 43);
+            textBox4.TabIndex = 7;
+            textBox4.Enter += textBox4_Enter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(434, 47);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 37);
+            label6.TabIndex = 6;
+            label6.Text = "Price";
+            label6.Click += label6_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.Location = new Point(168, 47);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(200, 43);
+            textBox3.TabIndex = 5;
+            textBox3.KeyDown += textBox3_KeyDown;
+            textBox3.KeyUp += textBox3_KeyUp;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(18, 50);
+            label5.Name = "label5";
+            label5.Size = new Size(119, 37);
+            label5.TabIndex = 1;
+            label5.Text = "Product";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(25, 520);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.RowTemplate.Height = 41;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(1258, 530);
+            dataGridView1.TabIndex = 2;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(1327, 629);
+            label9.Name = "label9";
+            label9.Size = new Size(188, 37);
+            label9.TabIndex = 9;
+            label9.Text = "Grand Total : ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(1544, 629);
+            label10.Name = "label10";
+            label10.Size = new Size(33, 37);
+            label10.TabIndex = 10;
+            label10.Text = "0";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(1327, 758);
+            button1.Name = "button1";
+            button1.Size = new Size(279, 54);
+            button1.TabIndex = 11;
+            button1.Text = "Delete";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(1327, 872);
+            button2.Name = "button2";
+            button2.Size = new Size(385, 62);
+            button2.TabIndex = 12;
+            button2.Text = "Save and Print Bill";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // sales
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1860, 1082);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             Name = "sales";
             Text = "sales";
+            Load += sales_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -157,5 +355,21 @@
         private Label label2;
         private Label label1;
         private DateTimePicker dateTimePicker1;
+        private Panel panel2;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox6;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label6;
+        private ListBox listBox1;
+        private DataGridView dataGridView1;
+        private Label label9;
+        private Label label10;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- sales.cs
+++ sales.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace InventoryManagement
 {
     public partial class sales : Form
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\shres\source\repos\InventoryManagement\inventory.mdf;Integrated Security=True");
+
+        DataTable datTab = new DataTable();
+
+        int total = 0;
+
         public sales()
         {
             InitializeComponent();
@@ -34,6 +42,213 @@ namespace InventoryManagement
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sales_Load(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn.Open();
+            datTab.Clear();
+            datTab.Columns.Add("Product");
+            datTab.Columns.Add("Price");
+            datTab.Columns.Add("Quantity");
+            datTab.Columns.Add("Total");
+
+        }
+
+        private void textBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+            listBox1.Visible = true;
+
+            listBox1.Items.Clear();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from stock where product_name like('" + textBox3.Text + "%')";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                listBox1.Items.Add(dr["product_name"].ToString());
+            }
+
+        }
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                listBox1.Focus();
+                listBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                this.listBox1.SelectedIndex = this.listBox1.SelectedIndex + 1;
+
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                this.listBox1.SelectedIndex = this.listBox1.SelectedIndex - 1;
+
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox3.Text = listBox1.SelectedItem.ToString();
+                listBox1.Visible = false;
+                textBox4.Focus();
+
+            }
+
+        }
+
+        private void textBox4_Enter(object sender, EventArgs e)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select top 1 * from purchase_master where product_name='" + textBox3.Text + "' order by id desc";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                textBox4.Text = dr["product_price"].ToString();
+            }
+
+        }
+
+        private void textBox5_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox6.Text = Convert.ToString(Convert.ToInt32(textBox4.Text) * Convert.ToInt32(textBox5.Text));
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int stock = 0;
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from stock where product_name='" + textBox3.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                stock = Convert.ToInt32(dr["product_qty"].ToString());
+            }
+
+            if (Convert.ToInt32(textBox5.Text) > stock)
+            {
+                MessageBox.Show("This much quantity is not available in the stock!");
+            }
+            else
+            {
+                DataRow dr = datTab.NewRow();
+                dr["Product"] = textBox3.Text;
+                dr["Price"] = textBox4.Text;
+                dr["Quantity"] = textBox5.Text;
+                dr["Total"] = textBox6.Text;
+                datTab.Rows.Add(dr);
+                dataGridView1.DataSource = datTab;
+
+                total = total + Convert.ToInt32(dr["Total"].ToString());
+
+                label10.Text = Convert.ToString(total);
+
+            }
+
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                total = 0;
+                datTab.Rows.RemoveAt(Convert.ToInt32(dataGridView1.CurrentCell.RowIndex.ToString()));
+
+                foreach (DataRow dr in datTab.Rows)
+                {
+                    total = total + Convert.ToInt32(dr["Total"].ToString());
+                }
+                label10.Text = Convert.ToString(total);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string orderId = "";
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into order_user values('" + textBox1.Text + "', '" + textBox2.Text + "', '" + comboBox1.Text + "', '" + dateTimePicker1.Value.ToString("dd/mm/yyyy") + "')";
+            cmd.ExecuteNonQuery();
+
+            SqlCommand cmd2 = conn.CreateCommand();
+            cmd2.CommandType = CommandType.Text;
+            cmd2.CommandText = "select top 1 * from order_user order by id desc";
+            cmd2.ExecuteNonQuery();
+
+            DataTable dt2 = new DataTable();
+            SqlDataAdapter da2 = new SqlDataAdapter(cmd2);
+            da2.Fill(dt2);
+            foreach (DataRow dr in dt2.Rows)
+            {
+                orderId = dr["id"].ToString();
+            }
+
+
+            foreach (DataRow dr in datTab.Rows)
+            {
+                SqlCommand cmd3 = conn.CreateCommand();
+                cmd3.CommandType = CommandType.Text;
+                cmd3.CommandText = "insert into order_item values('" + orderId.ToString() + "', '" + dr["Product"].ToString() + "', '" + dr["Price"].ToString() + "', '" + dr["Quantity"].ToString() + "', '" + dr["Total"].ToString() + "')";
+                cmd3.ExecuteNonQuery();
+            }
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            label10.Text = "0";
+
+            datTab.Clear();
+            dataGridView1.DataSource = datTab;
+
+            MessageBox.Show("Record inserted successfully!");
 
         }
     }
